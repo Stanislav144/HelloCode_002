d9f11f87990e4c3897f21a71251c145b1cc65a6d
50_Satoshi <celovekzivoj18@gmail.com>
2022-08-17 21:40:32 +0300

    Example 005_Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Искрена")
+{
+    Console.WriteLine("Ура, это же ИСКРЕНА!");    
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
+
